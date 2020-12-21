4fffae06c31612dd39026f52a6dcaa9429431a2e
Michael Adrah <43325182+michaeldelali@users.noreply.github.com>
2020-12-21 14:44:39 +0000

    FIRST COMMIT

---
+++ api/models/item.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose'); // Erase if already required
+
+// Declare the Schema of the Mongo model
+var itemSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        // required:true,
+        index:true,
+    },
+    description:{
+        type:String,
+        // required:true,
+    },
+    category:{
+        type:String,
+        // required:true,
+    },
+    instock:{
+        type:Number,
+        // require:true,
+    },
+    infield:{
+        type:Number,
+        // require:true,
+    }
+    },
+    {
+        timestamps:true,
+    });
+
+//Export the model
+module.exports = mongoose.model('Item', itemSchema);
---
+++ api/routes/items.js
@@ -0,0 +1,57 @@
+const  router  = require('express').Router();
+let Item = require('../models/item.model');
+
+router.route('/').get((req,res)=>{
+    Item.find()
+    .then(items => res.json(items))
+    .catch(err => res.status(400).json('Error: ' +err));
+});
+
+//get data with id
+router.route('/:id').get((req, res) => {
+    Item.findById(req.params.id)
+      .then(items => res.json(items))
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
+
+router.route('/add').post((req,res)=>{
+    const name = req.body.name;
+    const description = req.body.description;
+    const category = req.body.category;
+    const instock = req.body.instock;
+    const infield = req.body.infield;
+    // const instock = Number(req.body.instock);
+    // const infield = Number(req.body.infield);
+
+
+const newItem = new Item({
+    name,
+    description,
+    category,
+    instock,
+    infield,
+});
+
+newItem.save()
+.then(() => res.json('Item added!'))
+.catch(err=> res.status(400).json('Error: '+ err));
+});
+
+// update items
+router.route('/update/:id').post((req,res)=>{
+    Item.findById(req.params.id)
+    .then(item =>{
+        // item.name = req.body.name;
+        // item.description = req.body.description;
+        // item.category = req.body.category;
+        item.instock = req.body.instock;
+        item.infield = req.body.infield;
+        item.save()
+        .then(() => res.json('Item Updated'))
+        .catch(err => res.status(400).json('Error: ' +err));
+    })
+    .catch(err => res.status(400).json('Error' + err));
+});
+
+
+module.exports = router;
